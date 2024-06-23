b1e5ba44d4571194b1115ebf69f6f6ed41f48c77
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-06-23 13:09:02 +0300

    Add demo 2 with js syntax

--- Intro to React and JSX/demo-2/script.js
+++ Intro to React and JSX/demo-2/script.js
@@ -2,8 +2,8 @@ let rootElement = document.getElementById('root');
 
 let reactElement = React.createElement(
     'header', 
-    {}, 
-    React.createElement('h1', {}, 'Hello React!'),
+    { className: 'site-header'}, 
+    React.createElement('h1', { id: 'main-heading' }, 'Hello React!'),
     React.createElement('h2', {}, 'React is the best!')
 );
 
