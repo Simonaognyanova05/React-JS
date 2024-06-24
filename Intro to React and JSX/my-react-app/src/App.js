b27d113ef7e5a081f373374b11d7bdbb6794e203
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-06-24 22:08:30 +0300

    Add blog component

--- Intro to React and JSX/my-react-app/src/App.js
+++ Intro to React and JSX/my-react-app/src/App.js
@@ -3,6 +3,7 @@ import Slider from './components/Slider';
 import Shop from './components/Shop';
 import About from './components/About';
 import Offer from './components/Offer';
+import Blog from './components/Blog';
 import './App.css';
 
 function App() {
@@ -13,62 +14,7 @@ function App() {
       <Shop />
       <About />
       <Offer />
-
-      <section class="blog_section ">
-        <div class="container">
-          <div class="heading_container">
-            <h2>
-              Latest From Blog
-            </h2>
-          </div>
-          <div class="row">
-            <div class="col-md-6">
-              <div class="box">
-                <div class="img-box">
-                  <img src="images/b1.jpg" alt="" />
-                  <h4 class="blog_date">
-                    14 <br />
-                    July
-                  </h4>
-                </div>
-                <div class="detail-box">
-                  <h5>
-                    Molestiae ad reiciendis dignissimos
-                  </h5>
-                  <p>
-                    alteration in some form, by injected humour, or randomised words which don't look even slightly believable.
-                  </p>
-                  <a href="">
-                    Read More
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-6">
-              <div class="box">
-                <div class="img-box">
-                  <img src="images/b2.jpg" alt="" />
-                  <h4 class="blog_date">
-                    15 <br />
-                    July
-                  </h4>
-                </div>
-                <div class="detail-box">
-                  <h5>
-                    Dolores vel maiores voluptatem enim
-                  </h5>
-                  <p>
-                    alteration in some form, by injected humour, or randomised words which don't look even slightly believable.
-                  </p>
-                  <a href="">
-                    Read More
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
+      <Blog />
 
       <section class="client_section layout_padding">
         <div class="container">
