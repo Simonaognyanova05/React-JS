ec2b527f82757554cb9cc0ec25cb9db9d466e91e
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-06-24 21:56:29 +0300

    Add slider component

--- Intro to React and JSX/my-react-app/src/App.js
+++ Intro to React and JSX/my-react-app/src/App.js
@@ -1,36 +1,13 @@
 import Header from './components/Header';
+import Slider from './components/Slider';
 import './App.css';
 
 function App() {
   return (
     <div>
       <Header/>
-
-      <section class="slider_section position-relative">
-        <div class="slider_bg_box">
-          <img src="images/slider-bg.jpg" alt="" />
-        </div>
-        <div class="slider_bg"></div>
-        <div class="container">
-          <div class="col-md-9 col-lg-8">
-            <div class="detail-box">
-              <h1>
-                Best Jewellery
-                <br /> Collection
-              </h1>
-              <p>
-                It is a long established fact that a reader will be distracted by the readable content of a page when
-                looking at its layout. The point of using Lorem
-              </p>
-              <div>
-                <a href="" class="slider-link">
-                  Shop Now
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
+      <Slider />
+     
 
       <section class="shop_section layout_padding">
         <div class="container">
