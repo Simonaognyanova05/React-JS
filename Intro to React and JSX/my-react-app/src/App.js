9b513f84509227ff739b2b8dcfecf231352a54f0
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-06-24 22:06:47 +0300

    Add offer component

--- Intro to React and JSX/my-react-app/src/App.js
+++ Intro to React and JSX/my-react-app/src/App.js
@@ -2,6 +2,7 @@ import Header from './components/Header';
 import Slider from './components/Slider';
 import Shop from './components/Shop';
 import About from './components/About';
+import Offer from './components/Offer';
 import './App.css';
 
 function App() {
@@ -11,50 +12,7 @@ function App() {
       <Slider />
       <Shop />
       <About />
-
-      <section class="offer_section layout_padding">
-        <div class="container-fluid">
-          <div class="row">
-            <div class="col-md-7 px-0">
-              <div class="box offer-box1">
-                <img src="images/o1.jpg" alt="" />
-                <div class="detail-box">
-                  <h2>
-                    Upto 15% Off
-                  </h2>
-                  <a href="">
-                    Shop Now
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-5 px-0">
-              <div class="box offer-box2">
-                <img src="images/o2.jpg" alt="" />
-                <div class="detail-box">
-                  <h2>
-                    Upto 10% Off
-                  </h2>
-                  <a href="">
-                    Shop Now
-                  </a>
-                </div>
-              </div>
-              <div class="box offer-box3">
-                <img src="images/o3.jpg" alt="" />
-                <div class="detail-box">
-                  <h2>
-                    Upto 20% Off
-                  </h2>
-                  <a href="">
-                    Shop Now
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
+      <Offer />
 
       <section class="blog_section ">
         <div class="container">
