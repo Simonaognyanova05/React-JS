0d50e4a47a98d14b514295f0aec15a6973e95c3f
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-06-27 13:35:09 +0300

    Add static list to the first demo

---
+++ Components - Basic Idea/demo-1/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>ToDo App</h1>
+
+        <main>
+          <h2>Tasks</h2>
+          <ul>
+            <li>Clean</li>
+            <li>Go shopping</li>
+            <li>Learn React</li>
+          </ul>
+        </main>
+
+        <footer>All rights resieved. &copy;</footer>
+      </header>
+    </div>
+  );
+}
+
+export default App;
