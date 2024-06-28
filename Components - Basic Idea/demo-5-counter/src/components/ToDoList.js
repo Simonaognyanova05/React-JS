cda592dcc4d58399af310e9d5129ff93493f5a11
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-06-28 15:02:25 +0300

    Add dynamic input

--- Components - Basic Idea/demo-5-counter/src/components/ToDoList.js
+++ Components - Basic Idea/demo-5-counter/src/components/ToDoList.js
@@ -3,16 +3,23 @@ import ToDoListItem from "./ToDoListItem";
 
 function ToDoList() {
     let countState = React.useState(0);
+    let nameState = React.useState('');
 
     let [count, setCount] = countState;
+    let [name, setName] = nameState;
+
+    const changeName = (e) => {
+        setName(e.target.value);
+    }
     return (
         <>
-            <h2>Counter</h2>
+            <h2>Counter - {name}</h2>
 
             <ul>
                 <ToDoListItem>{count}</ToDoListItem>
             </ul>
-
+            {/* dynamic input */}
+            <input type='text' onChange={changeName} />
             {/* events */}
             <button onClick={() => setCount(count + 1)}>Test</button >
         </>
