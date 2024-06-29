4db576160fe2747ba1ba5641e7c5f8bf017a8df9
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-06-29 13:02:10 +0300

    Add task-1 with advanced array

--- Components - Basic Idea/task-1/src/components/Main.js
+++ Components - Basic Idea/task-1/src/components/Main.js
@@ -3,14 +3,17 @@ import ListItem from "./ListItem";
 
 
 export default function Main() {
-    let nameState = React.useState(['Simona', 'Nadeto', 'Ivana', 'Dimeto']);
+    // basic array
+    // let nameState = React.useState(['Simona', 'Nadeto', 'Ivana', 'Dimeto']);
+
+    //advanced array
+    let nameState = React.useState([{ name: 'Simona', years: 19 }, { name: 'Nadeto', years: 18 }, { name: 'Ivana', years: 19 }]);
 
     let [name, setNames] = nameState;
     return (
         <main>
             <ul>
-
-                {name.map(x => <ListItem>{x}</ListItem>)}
+                {name.map(x => <ListItem>{x.name} - {x.years}</ListItem>)}
             </ul>
         </main>
     );
