cab15e9046a3c73670d1866309773aa33835d2a1
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-07-06 10:40:26 +0300

    Extract service

--- Workshop-Components/GamesPlay/src/components/gameCatalog/CatalogPage.js
+++ Workshop-Components/GamesPlay/src/components/gameCatalog/CatalogPage.js
@@ -1,26 +1,25 @@
 import { useEffect, useState } from 'react';
 import CatalogTemplate from './CatalogTemplate';
+import { getAll } from '../../services/gameService'
 
 export default function CatalogPage() {
 
-    let [ games, setGames ] = useState([]);
+    let [games, setGames] = useState([]);
 
     useEffect(() => {
-        fetch('http://localhost:3030/data/games?sortBy=_createdOn%20desc')
-        .then(res => res.json())
-        .then(gamesResult => {
-            setGames(gamesResult);
-            console.log(gamesResult);
-        })
+        getAll()
+            .then(gamesResult => {
+                setGames(gamesResult);
+            })
     })
     return (
         <section id="catalog-page">
             <h1>All Games</h1>
 
-            {games.length > 0 ? games.map(x => <CatalogTemplate key={x._id} game={x}/>) : <h3 className="no-articles">No articles yet</h3>}
+            {games.length > 0 ? games.map(x => <CatalogTemplate key={x._id} game={x} />) : <h3 className="no-articles">No articles yet</h3>}
 
 
-           
+
         </section>
     );
 }
