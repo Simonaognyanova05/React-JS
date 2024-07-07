d23b3285e237a27d2d98160d0f4d88f0c8c89530
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-07-07 11:38:51 +0300

    Export components

---
+++ Workshop-Components/games/src/App.js
@@ -0,0 +1,17 @@
+import Header from "./components/Header";
+import Home from "./components/Home";
+
+function App() {
+  return (
+    <div id="box">
+
+      <Header />
+
+      <main id="main-content">
+        <Home />
+      </main>
+    </div>
+  );
+}
+
+export default App;
