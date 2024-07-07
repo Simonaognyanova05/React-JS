0128fd649588aa9bf8a511997619984ff3239e31
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-07-07 16:53:10 +0300

    Export components

---
+++ Workshop-Components/luxury-cars/src/App.js
@@ -0,0 +1,16 @@
+import Header from "./components/Header";
+import Home from "./components/Home";
+
+function App() {
+  return (
+    <div id="wrapper">
+      <Header />
+
+      <main id="main-element">
+        <Home />
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ Workshop-Components/luxury-cars/src/components/Home.js
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+        <section id="hero">
+            <h1>
+                Accelerate Your Passion Unleash the Thrill of Sport Cars Together!
+            </h1>
+        </section>
+    );
+}
