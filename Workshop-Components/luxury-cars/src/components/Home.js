b8091c5710c42c679af3ef11e17b49f02b388e9c
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-07-08 12:33:08 +0300

    Remove the last task

--- Workshop-Components/luxury-cars/src/components/Home.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-    return (
-        <section id="hero">
-            <h1>
-                Accelerate Your Passion Unleash the Thrill of Sport Cars Together!
-            </h1>
-        </section>
-    );
-}
