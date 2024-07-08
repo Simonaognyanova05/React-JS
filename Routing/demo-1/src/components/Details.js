b88ee8bee47ad95b8831791fcf97f554344b5d45
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-07-08 16:39:43 +0300

    Add my rounting

--- Routing/demo-1/src/components/Details.js
+++ Routing/demo-1/src/components/Details.js
@@ -2,17 +2,16 @@ import { useState, useEffect } from "react";
 
 export default function Details({
     match
-}) {
+}){
     let [game, setGame] = useState([]);
 
     useEffect(() => {
         fetch(`http://localhost:3030/data/games/${match.params.gameId}`)
-            .then(res => res.json())
-            .then(result => {
-                setGame(result);
-            })
-    }, []);
-
+        .then(res => res.json())
+        .then(result => {
+            setGame(result);
+        })
+    }, [])
     return (
         <p>{game.title}</p>
     );
