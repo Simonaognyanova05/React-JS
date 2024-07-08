9f67e3e428ed24041adea286ec752ad9114a4d70
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-07-08 17:05:55 +0300

    Add one way for redirecting

--- Routing/demo-1/src/components/Details.js
+++ Routing/demo-1/src/components/Details.js
@@ -1,18 +1,33 @@
 import { useState, useEffect } from "react";
 
 export default function Details({
-    match
-}){
+    match,
+    history
+}) {
     let [game, setGame] = useState([]);
 
     useEffect(() => {
         fetch(`http://localhost:3030/data/games/${match.params.gameId}`)
-        .then(res => res.json())
-        .then(result => {
-            setGame(result);
-        })
+            .then(res => res.json())
+            .then(result => {
+                setGame(result);
+            })
     }, [])
+
+    const onSubmitHandler = (e) => {
+        e.preventDefault();
+
+        history.push('/');
+    }
     return (
-        <p>{game.title}</p>
+        <>
+            <p>{game.title}</p>
+
+            <form onSubmit={onSubmitHandler}>
+                <input type="text" />
+                <input type="submit" value="submit" />
+
+            </form>
+        </>
     );
 }
