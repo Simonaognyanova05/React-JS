5f62e3850b27a8371c4b1ed533f099b3e76aa140
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-07-10 16:37:56 +0300

    Export components

---
+++ Routing/Luxury-cars/src/App.js
@@ -0,0 +1,11 @@
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div id="wrapper">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ Routing/Luxury-cars/src/components/Header.js
@@ -0,0 +1,24 @@
+export default function Header() {
+    return (
+        <header>
+            <a id="logo" href="#"><img id="logo-car" src="./images/car-logo.png" alt="img" /></a>
+            <nav>
+                <div>
+                    <a href="#">Our Cars</a>
+                    <a href="#">Search</a>
+                </div>
+
+                <div class="user">
+                    <a href="#">Add Your Car</a>
+                    <a href="#">Logout</a>
+                </div>
+
+                <div class="guest">
+                    <a href="#">Login</a>
+                    <a href="#">Register</a>
+                </div>
+            </nav>
+        </header>
+
+    );
+}
