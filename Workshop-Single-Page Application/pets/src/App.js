f757e85cc432559d0b20064e789c7faffbf6a8b6
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-07-11 21:28:40 +0300

    Export components

---
+++ Workshop-Single-Page Application/pets/src/App.js
@@ -0,0 +1,18 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div id="container">
+      <Header />
+
+      <main id="site-content">
+
+      </main>
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ Workshop-Single-Page Application/pets/src/components/Header.js
@@ -0,0 +1,22 @@
+export default function Header() {
+    return (
+        <header id="site-header">
+            <nav className="navbar">
+                <section className="navbar-dashboard">
+                    <a href="#">Dashboard</a>
+
+                    <div id="guest">
+                        <a className="button" href="#">Login</a>
+                        <a className="button" href="#">Register</a>
+                    </div>
+                    <div id="user">
+                        <span>Welcome, email</span>
+                        <a className="button" href="#">My Books</a>
+                        <a className="button" href="#">Add Book</a>
+                        <a className="button" href="#">Logout</a>
+                    </div>
+                </section>
+            </nav>
+        </header>
+    );
+}
