d410dc4be6e6fd85f96bd2619e9e48e5103ad7dc
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-07-12 12:45:41 +0300

    Add demo-1, simple way for getting data from form

---
+++ Forms/demo-1/src/App.js
@@ -0,0 +1,33 @@
+function App() {
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    let formData = new FormData(e.currentTarget);
+
+    let username = formData.get('username');
+    let password = formData.get('password');
+
+
+    console.log(username);
+    console.log(password);
+  }
+
+  return (
+    <div className="App">
+      <form method="POST" onSubmit={submitHandler}>
+        <div>
+          <label htmlFor="username">Username</label>
+          <input type="text" name="username" id="username" />
+        </div>
+
+        <div>
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" id="password" />
+        </div>
+        <input type="submit" value="Login" />
+      </form>
+    </div>
+  );
+}
+
+export default App;
