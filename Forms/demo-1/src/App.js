9b0630f656bdb900f2f5be54143475fa25b81d58
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-07-12 13:17:53 +0300

    Add validation into demo-1

--- Forms/demo-1/src/App.js
+++ Forms/demo-1/src/App.js
@@ -1,16 +1,30 @@
+import { useState } from 'react';
+
 function App() {
   const submitHandler = (e) => {
     e.preventDefault();
 
     let formData = new FormData(e.currentTarget);
 
-    let {username, password} = Object.fromEntries(formData);
-    // let username = formData.get('username');
-    // let password = formData.get('password');
+    //let {username, password} = Object.fromEntries(formData);
+    let username = formData.get('username');
+    let password = formData.get('password');
 
 
     console.log(username);
     console.log(password);
+  };
+
+  let [isValid, setIsValid] = useState(false);
+
+  const changeName = (e) => {
+    let changeName = e.target.value;
+
+    if (changeName.length < 3) {
+      setIsValid(false);
+    } else {
+      setIsValid(true);
+    }
   }
 
   return (
@@ -18,13 +32,20 @@ function App() {
       <form method="POST" onSubmit={submitHandler}>
         <div>
           <label htmlFor="username">Username</label>
-          <input type="text" name="username" id="username" />
+          <input type="text" name="username" id="username" defaultValue="Pesho" onChange={changeName} />
+          {isValid ? '' : <span>The input is invalid!</span>}
         </div>
 
         <div>
           <label htmlFor="password">Password</label>
-          <input type="password" name="password" id="password" />
+          <input type="password" name="password" id="password" defaultValue="123" />
         </div>
+
+        <div>
+          <label htmlFor="isAdmin">Is Admin</label>
+          <input type="checkbox" name="isAdmin" id="isAdmin" defaultChecked />
+        </div>
+
         <input type="submit" value="Login" />
       </form>
     </div>
@@ -32,3 +53,4 @@ function App() {
 }
 
 export default App;
+
