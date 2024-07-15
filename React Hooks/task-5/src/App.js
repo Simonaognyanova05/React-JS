7f56a2e1eb256a3f0ed290548c545390c7db69a4
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-07-15 16:44:00 +0300

    Add task-5, unmount and useEffect

--- React Hooks/task-5/src/App.js
+++ React Hooks/task-5/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  let [name, setName] = useState('');
+
+
+  useEffect(() => {
+    return (() => {
+      console.log('unmount');
+    })
+  }, [])
+
+  const changeHandler = (e) => {
+    e.preventDefault();
+    setName(e.currentTarget.value);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type='text' onChange={changeHandler} />
+
+      {name == 'Simona'
+        ? <p>{name}</p>
+        : ''
+      }
     </div>
   );
 }
