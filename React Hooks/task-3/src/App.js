c7c70249233fd804458757a5a273d4b3e5df0ea7
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-07-15 16:14:53 +0300

    Add task-3, object into state

--- React Hooks/task-3/src/App.js
+++ React Hooks/task-3/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  let [info, setInfo] = useState({
+    name: 'Simona',
+    age: 19
+  });
+
+  useEffect(() => {
+    setTimeout(() => {
+      setInfo(state => ({
+        ...state,
+        name: 'Magi',
+        age: 13
+      }))
+    }, 2000)
+  }, []);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{info.name} - {info.age}</p>
     </div>
   );
 }
