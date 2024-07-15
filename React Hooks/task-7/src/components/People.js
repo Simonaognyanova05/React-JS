51d47e4807f7559877c59bfae3ee7dd4955c3b2c
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-07-15 17:39:51 +0300

    Add task-7, add people request

--- React Hooks/task-7/src/components/People.js
+++ React Hooks/task-7/src/components/People.js
@@ -1,5 +1,16 @@
-export default function People(){
+import useFetch from "../hook/useFetch";
+
+export default function People() {
+    const url = 'http://localhost:3030/jsonstore/people';
+
+    const people = useFetch(url);
+
     return (
-        <h1>People page</h1>
+        <>
+            <h1>People page</h1>
+            <ul>
+                {people.map(x => <li key={x._id}>{x.name} - {x.years}</li>)}
+            </ul>
+        </>
     );
 }
