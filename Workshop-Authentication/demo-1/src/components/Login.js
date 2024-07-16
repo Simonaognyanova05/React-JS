0d5f4019ab0417a85e84602c042c2c2943f3df7e
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-07-16 13:32:17 +0300

    Throw error if data is invalid

--- Workshop-Authentication/demo-1/src/components/Login.js
+++ Workshop-Authentication/demo-1/src/components/Login.js
@@ -12,6 +12,9 @@ export default function Login() {
             .then(res => {
                 console.log(res);
             })
+            .catch(err => {
+                console.log(err);
+            })
     }
     return (
         <form onSubmit={submitHandler}>
--- Workshop-Authentication/demo-1/src/services/login.js
+++ Workshop-Authentication/demo-1/src/services/login.js
@@ -1,10 +1,17 @@
-export function login(email, password) {
-    return fetch('http://localhost:3030/users/login', {
+export async function login(email, password) {
+    let loginData = await fetch('http://localhost:3030/users/login', {
         method: 'POST',
         headers: {
             'content-type': 'applicaion/json'
         },
         body: JSON.stringify({ email, password })
-    })
-        .then(res => res.json())
+    });
+
+    let res = await loginData.json();
+
+    if(loginData.ok){
+        return res;
+    }else{
+        throw res.message;
+    }
 }
