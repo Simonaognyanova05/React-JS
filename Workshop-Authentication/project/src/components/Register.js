ab4dd981dd27c0905bb75acbb76acbb8e10d5bcd
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-07-17 22:49:14 +0300

    Add register request

--- Workshop-Authentication/project/src/components/Register.js
+++ Workshop-Authentication/project/src/components/Register.js
@@ -1,15 +1,35 @@
+import { Link, useNavigate } from 'react-router-dom';
+import { useContext } from 'react';
+import { register } from '../services/register';
+import { AuthContext } from '../contexts/AuthContext';
+
 export default function Register() {
+    let navigate = useNavigate();
+    let { onRegister } = useContext(AuthContext);
+    const registerHandler = async (e) => {
+        e.preventDefault();
+
+        let formData = new FormData(e.currentTarget);
+        let { email, password } = Object.fromEntries(formData);
+
+        let user = await register(email, password);
+
+        if (user) {
+            onRegister(user);
+            navigate('/');
+        }
+    }
     return (
         <section id="register">
-            <div class="form">
-                <img class="border" src="./images/border.png" alt="" />
+            <div className="form">
+                <img className="border" src="/images/border.png" alt="" />
                 <h2>Register</h2>
-                <form class="register-form">
+                <form className="register-form" onSubmit={registerHandler}>
                     <input type="text" name="email" id="register-email" placeholder="email" />
                     <input type="password" name="password" id="register-password" placeholder="password" />
-                    <input type="password" name="re-password" id="repeat-password" placeholder="repeat password"/>
+                    <input type="password" name="re-password" id="repeat-password" placeholder="repeat password" />
                     <button type="submit">register</button>
-                    <p class="message">Already registered? <a href="#">Login</a></p>
+                    <p className="message">Already registered? <Link to="/login">Login</Link></p>
                 </form>
             </div>
         </section>
