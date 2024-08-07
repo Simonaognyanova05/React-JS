62b8dc81fb7892e163b598c810aad3965c19cb61
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-08-07 13:34:13 +0300

    Add post proccessing request

--- Exercises/server/index.js
+++ Exercises/server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 
 const cars = require('./src/cars');
+const { createCar } = require('./services/createCars');
 
 const app = express();
 
@@ -10,6 +11,10 @@ app.use(express.json());
 
 app.get('/cars', async (req, res) => {
     await cars();
+});
+
+app.post('/create', async (req, res) => {
+    await createCar(req, res);
 })
 
 app.listen(3001);
---
+++ Exercises/server/services/createCars.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Car = require('./models/Car');
+
+const dbUrl = 'mongodb+srv://simonaognanova05:NNN9BLA68lHrjlMM@react-task.hujfarb.mongodb.net/';
+
+const connectionParams = {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+};
+
+async function createCar(req, res) {
+    await mongoose.connect(dbUrl, connectionParams);
+
+    const { brand, model, year } = req.body;
+
+    try {
+        const car = new Car({ brand, model, year });
+
+        await car.save();
+    } catch (e) {
+        throw e; F
+    }
+};
+
+module.exports = { createCar };
