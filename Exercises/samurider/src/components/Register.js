40a6d838530e0b891f589bf3cf96eb16472da8f0
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-08-08 12:24:28 +0300

    Add post request to the back-end part

--- Exercises/samurider/src/components/Register.js
+++ Exercises/samurider/src/components/Register.js
@@ -6,7 +6,7 @@ export default function Register() {
                 <form className="register-form">
                     <input type="text" name="email" id="register-email" placeholder="email" />
                     <input type="password" name="password" id="register-password" placeholder="password" />
-                    <input type="password" name="re-password" id="repeat-password" placeholder="repeat password" />
+                    <input type="password" name="rePassword" id="repeat-password" placeholder="repeat password" />
                     <button type="submit">register</button>
                     <p className="message">Already registered? <a href="#">Login</a></p>
                 </form>
