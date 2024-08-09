9c9d7fcccb33294c844087a248d846226bae38d2
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-08-09 17:42:49 +0300

    Add login with mongoDb

---
+++ Exercises-2/login/server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+
+const { login } = require('./services/login');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.post('/login', async (req, res) => {
+    await login(req, res);
+})
+
+app.listen(2005);
--- Exercises-2/login/src/components/Login.js
+++ Exercises-2/login/src/components/Login.js
@@ -1,9 +1,28 @@
-export default function Login(){
+import { useNavigate } from 'react-router-dom';
+import { login } from '../services/login';
+
+export default function Login() {
+    const navigate = useNavigate();
+
+    const loginHandler = async (e) => {
+        e.preventDefault();
+
+        let formData = new FormData(e.currentTarget);
+        let { email, password } = Object.fromEntries(formData);
+
+        let res = await login(email, password);
+
+        if (res.status == 200) {
+            navigate('/');
+        } else {
+            console.log('Incorrect data!');
+        }
+    }
     return (
-        <form>
-            <input type="text" name="email" placeholder="email"/>
-            <input type="password" name="password" placeholder="password"/>
-            <input type="submit" name="Login"/>
+        <form onSubmit={loginHandler}>
+            <input type="text" name="email" placeholder="email" />
+            <input type="password" name="password" placeholder="password" />
+            <input type="submit" name="Login" />
         </form>
     );
 }
