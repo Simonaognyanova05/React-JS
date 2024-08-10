d3df32b0bf393f5327a10b76b4f52ef2de40ba88
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-08-10 14:00:11 +0300

    Add logout to the project

--- Exercises-2/login/src/components/Logout.js
+++ Exercises-2/login/src/components/Logout.js
@@ -1,5 +1,15 @@
-export default function Logout(){
-    return (
-        <h1>Logout</h1>
-    );
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../contexts/AuthContext";
+
+export default function Logout() {
+    const navigate = useNavigate();
+
+    const { onLogout } = useAuth();
+
+    useEffect(() => {
+        onLogout();
+        navigate('/');
+    }, [])
+    return null;
 }
