35071bff84c859085cd8374f67824f3f07fb4f9f
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-08-10 14:28:10 +0300

    Remove samurider

--- Exercises-2/samurider/src/components/Home.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-    return (
-        <section id="home">
-            <h1>Welcome to <span>Samurider</span> moto market, your premier destination for Japanese motorcycles.</h1>
-            <img src="./images/motorcycle.png" alt="home" />
-        </section>
-    );
-}
