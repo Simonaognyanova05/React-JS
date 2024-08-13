1b63898542ebd29da34291aef6c758b7fa0ce6fd
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-08-13 17:16:01 +0300

    Add register to server

---
+++ Basic functional tasks/tattoo-masters/server/services/register.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const User = require('./models/User');
+
+const dbUrl = 'mongodb+srv://simonaognanova05:NNN9BLA68lHrjlMM@react-task.hujfarb.mongodb.net/tattoo_db';
+
+const connectionParams = {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+};
+
+async function register(req, res) {
+    await mongoose.connect(dbUrl, connectionParams);
+
+    const { email, password } = req.body;
+
+    const existedUser = await User.findOne({ email });
+
+    if (existedUser) {
+        return res.status(409).json();
+    };
+
+    const hashedPass = await bcrypt.hash(password, 10);
+    try {
+        const user = new User({ email, hashedPass });
+        await user.save();
+    } catch (e) {
+        throw e;
+    }
+};
+
+module.exports = { register };
