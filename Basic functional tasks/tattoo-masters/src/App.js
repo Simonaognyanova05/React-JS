b7aeaeab13ee42ae280f5e2b2070d143a5453591
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-08-13 16:54:12 +0300

    Add routing

--- Basic functional tasks/tattoo-masters/src/App.js
+++ Basic functional tasks/tattoo-masters/src/App.js
@@ -1,4 +1,12 @@
+import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
+import Collection from './components/Collection';
+import Create from './components/Create';
+import Details from './components/Details';
+import Edit from './components/Edit';
+import Home from './components/Home';
+import Login from './components/Login';
+import Register from './components/Register';
 import Footer from './components/Footer';
 
 function App() {
@@ -8,6 +16,16 @@ function App() {
         <Header />
 
         <main>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/collection' element={<Collection />} />
+            <Route path='/create' element={<Create />} />
+            <Route path='/details' element={<Details />} />
+            <Route path='/edit' element={<Edit />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+
+          </Routes>
         </main>
       </div>
       <Footer />
--- Basic functional tasks/tattoo-masters/src/components/Header.js
+++ Basic functional tasks/tattoo-masters/src/components/Header.js
@@ -1,20 +1,22 @@
-export default function Header(){
-    return (
-        <header>
-          <a id="logo" href="#"><img id="logo-img" src="./images/logo.png" alt="logo" /></a>
-          <nav>
-            <a href="#">Collection</a>
+import { Link } from 'react-router-dom';
 
-            <div className="user">
-              <a href="#">Add Tattoo</a>
-              <a id="logout" href="#">Logout</a>
-            </div>
+export default function Header() {
+  return (
+    <header>
+      <Link id="logo" to="/"><img id="logo-img" src="/images/logo.png" alt="logo" /></Link>
+      <nav>
+        <Link to="/collection">Collection</Link>
 
-            <div className="guest">
-              <a href="#">Login</a>
-              <a href="#">Register</a>
-            </div>
-          </nav>
-        </header>
-    );
+        <div className="user">
+          <Link to="/create">Add Tattoo</Link>
+          <Link id="logout" to="/logout">Logout</Link>
+        </div>
+
+        <div className="guest">
+          <Link to="/login">Login</Link>
+          <Link to="/register">Register</Link>
+        </div>
+      </nav>
+    </header>
+  );
 }
