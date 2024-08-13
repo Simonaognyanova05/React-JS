2e4f470189863135f673c4740138c7e237e5eb65
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-08-13 18:04:02 +0300

    Check if user is logged

--- Basic functional tasks/tattoo-masters/src/components/Header.js
+++ Basic functional tasks/tattoo-masters/src/components/Header.js
@@ -1,21 +1,33 @@
 import { Link } from 'react-router-dom';
+import { useAuth } from '../contexts/AuthContext';
 
 export default function Header() {
+  const { user } = useAuth();
+
+  const guestHeader = (
+    <div className="guest">
+      <Link to="/login">Login</Link>
+      <Link to="/register">Register</Link>
+    </div>
+  );
+
+  const userHeader = (
+    <div className="user">
+      <Link to="/create">Add Tattoo</Link>
+      <Link id="logout" to="/logout">Logout</Link>
+    </div>
+  );
   return (
     <header>
       <Link id="logo" to="/"><img id="logo-img" src="/images/logo.png" alt="logo" /></Link>
       <nav>
         <Link to="/collection">Collection</Link>
 
-        <div className="user">
-          <Link to="/create">Add Tattoo</Link>
-          <Link id="logout" to="/logout">Logout</Link>
-        </div>
-
-        <div className="guest">
-          <Link to="/login">Login</Link>
-          <Link to="/register">Register</Link>
-        </div>
+        {
+          Boolean(user._id)
+            ? userHeader
+            : guestHeader
+        }
       </nav>
     </header>
   );
