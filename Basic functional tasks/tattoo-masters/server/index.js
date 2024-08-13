5b1a823efc371bcc95a683ac2ad68fefdc4c2395
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-08-13 17:51:33 +0300

    Add login to client side

--- Basic functional tasks/tattoo-masters/server/index.js
+++ Basic functional tasks/tattoo-masters/server/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const cors = require('cors');
+
 const { register } = require('./services/register');
-
-
+const { login } = require('./services/login');
 
 const app = express();
 app.use(cors());
@@ -10,6 +10,10 @@ app.use(express.json());
 
 app.post('/register', async (req, res) => {
     await register(req, res);
-})
+});
+
+app.post('/login', async (req, res) => {
+    await login(req, res);
+});
 
 app.listen(2005);
---
+++ Basic functional tasks/tattoo-masters/server/services/login.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const User = require('./models/User');
+
+const dbUrl = 'mongodb+srv://simonaognanova05:NNN9BLA68lHrjlMM@react-task.hujfarb.mongodb.net/tattoo_db';
+
+const connectionParams = {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+};
+
+async function login(req, res) {
+    await mongoose.connect(dbUrl, connectionParams);
+
+    const { email, password } = req.body;
+
+    try {
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(404).json();
+        };
+
+        const comparedPass = await bcrypt.compare(password, user.hashedPass);
+        if (!comparedPass) {
+            return res.status(401).json();
+        }
+
+        return res.status(200).json({ _id: user._id, email: user.email });
+    } catch (e) {
+        throw e;
+    }
+};
+
+module.exports = { login };
