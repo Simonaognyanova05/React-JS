358a99d7111756934fe0460326af4025463aa799
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-08-15 21:24:25 +0300

    Add login request to client side

--- Basic functional tasks/renew-tech-cleanup/src/components/Header.js
+++ Basic functional tasks/renew-tech-cleanup/src/components/Header.js
@@ -1,6 +1,22 @@
 import { Link } from "react-router-dom";
+import { useAuth } from '../contexts/AuthContext';
 
 export default function Header() {
+    const { user } = useAuth();
+
+    const guestResult = (
+        <div class="guest">
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+        </div>
+    );
+
+    const userResult = (
+        <div class="user">
+            <Link to="/create">Add Solution</Link>
+            <Link to="/logout">Logout</Link>
+        </div>
+    );
     return (
         <header>
             <Link id="logo" to="/"><img id="logo-img" src="/images/logo2.png" alt="logo" /></Link>
@@ -9,15 +25,11 @@ export default function Header() {
                     <Link to="/solutions">Solutions</Link>
                 </div>
 
-                <div class="user">
-                    <Link to="/create">Add Solution</Link>
-                    <Link to="/logout">Logout</Link>
-                </div>
-
-                <div class="guest">
-                    <Link to="/login">Login</Link>
-                    <Link to="/register">Register</Link>
-                </div>
+                {
+                    Boolean(user._id)
+                    ? userResult
+                    : guestResult 
+                }
             </nav>
         </header>
     );
