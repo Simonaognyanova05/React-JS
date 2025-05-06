86cc0d6fbbe6fc0ff45fd424a518f021f653d9bb
Simonaognyanova05 <simonaognanova05@gmail.com>
2025-05-06 22:14:20 +0300

    Add basic client side

---
+++ Tasks/movies/movies/src/App.js
@@ -0,0 +1,13 @@
+import { Route, Router } from "react-router-dom";
+import Header from "./components/Header";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
