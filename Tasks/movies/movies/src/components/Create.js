4b02380ba28673c467112d64b92ff696cf067bab
Simonaognyanova05 <simonaognanova05@gmail.com>
2025-05-13 17:43:02 +0300

    Add create form

--- Tasks/movies/movies/src/components/Create.js
+++ Tasks/movies/movies/src/components/Create.js
@@ -1,5 +1,14 @@
-export default function Create(){
+export default function Create() {
     return (
-        <h1>Create movie</h1>
+        <>
+            <h1>Create movie</h1>
+            <form>
+                <input type="text" name="title" placeholder="Title"/>
+                <input type="text" name="year" placeholder="Year"/>
+                <input type="text" name="time" placeholder="Time"/>
+
+                <button>Create</button>
+            </form>
+        </>
     );
 }
