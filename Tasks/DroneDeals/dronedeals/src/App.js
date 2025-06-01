0e2df7d12fa2e3fac857b1d41beddfb5037cfd7e
Simonaognyanova05 <simonaognanova05@gmail.com>
2025-06-01 13:11:33 +0300

    Add router

--- Tasks/DroneDeals/dronedeals/src/App.js
+++ Tasks/DroneDeals/dronedeals/src/App.js
@@ -1,4 +1,12 @@
+import { Routes, Route } from 'react-router-dom';
 import Footer from "./components/Footer";
+import Dashboard from './components/Dashboard';
+import Create from './components/Create';
+import Details from './components/Details';
+import Edit from './components/Edit';
+import Login from './components/Login';
+import Register from './components/Register';
+import Home from './components/Home';
 import Header from "./components/Header";
 
 function App() {
@@ -8,7 +16,15 @@ function App() {
         <Header />
 
         <main id="main-element">
-
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/dashboard' element={<Dashboard />} />
+            <Route path='/register' element={<Register />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/edit' element={<Edit />} />
+            <Route path='/details' element={<Details />} />
+            <Route path='/create' element={<Create />} />
+          </Routes>
         </main>
       </div>
       <Footer />
